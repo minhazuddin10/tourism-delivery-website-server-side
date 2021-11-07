4159e90aba0574c8237e45edaf0e008f9dcea42f
minhazuddin10 <minhaz112922@gmail.com>
2021-11-07 19:06:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+/* 
+const uri = "mongodb+srv://travel_agent:<password>@cluster0.ofxwj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+*/
+
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.ofxwj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('travel_agent');
+        const servicesCollection = database.collection('services');
+        const ordersCollection = database.collection('orders');
+
+
+        // GET services API 
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        // POST Add Service
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await servicesCollection.insertOne(service);
+            res.json(result);
+        })
+
+        // GET Single Service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        });
+
+        // GET Manage all orders API
+        app.get('/orders', async (req, res) => {
+            const cursor = ordersCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
+        // POST Add Orders API
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.json(result);
+        });
+
+        // DELETE single order API
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send("Travel agent server Running");
+})
+
+app.listen(port, () => {
+    console.log("Travel agent server port", port);
+})
